e39992cb9e99a8898d1028a435bf9f13953ddde8
Nicholas Stevens <nichstev@gmail.com>
2024-11-28 14:43:42 +0100

    Oink oink!

---
+++ oink.py
@@ -0,0 +1,14 @@
+from colorama import Fore
+import pyfiglet
+
+print(Fore.BLUE + "")
+print("    (\\____/)")
+print("    /" + Fore.RED + " @" + Fore.BLUE + "__" + Fore.RED + "@ " + Fore.BLUE + "\\")
+print("   (  (oo)  )      " + Fore.CYAN + "OINK! OINK!") 
+print(Fore.BLUE + "    `-.~~.-'")
+print(Fore.WHITE + "____" + Fore.BLUE + "// :: \\\\" + Fore.WHITE + "___________________________")
+print(Fore.BLUE + "   (!)=[]=(!)")    
+print(Fore.YELLOW + "== " + Fore.BLUE + "_\\__/\\__/_" + Fore.YELLOW + "== == == == == == == == ==")    
+print(Fore.WHITE + "__" + Fore.BLUE + "[___∞)(∞___]" + Fore.WHITE + "_________________________")
+print(Fore.GREEN + "^” ^”^^”^ ^””^ ^^””^^ ^”^”^^” ””^^”^ ^^")
+print("" + Fore.RESET)
